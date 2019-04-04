b963583d682e9f9fb3ec2db0773b82586631a3b9
randy <gx28ga@hotmail.com>
2019-04-04 18:10:17 +0800

    init cart

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
   <div id="app">
-    <Home></Home>
+    <Cart></Cart>
   </div>
 </template>
 
 <script>
-import Home from "./views/Home";
+import Cart from "./components/Cart";
 
 export default {
   name: 'app',
   components: {
-    Home
+    Cart
   }
 }
 </script>
 
 <style>
-/* #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+
 </style>
