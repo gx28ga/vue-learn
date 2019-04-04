7e163d3c8811d13757b7593d622d7f798f9757db
randy <gx28ga@hotmail.com>
2019-04-04 12:44:26 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+        <div><input type="text" v-model="mInput"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"home",
+    data(){
+        return {
+            msg:"abc",
+            mInput:"yes yes yes yes yes"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
